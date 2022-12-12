efa89406551c21e4914d69b8a1952b5fd58be81f
LAPTOP-RQBHQ9EN/Kent Angelo <cabanagkentangelo@gmail.com>
2022-12-12 20:52:37 +0800

    commit

--- FIFO_Queues.py
+++ FIFO_Queues.py
@@ -1,8 +1,15 @@
 from collections import deque
 
 class Queue:
-    def __init__(self):
-        self._elements = deque()
+    def __init__(self, *elements):
+        self._elements = deque(elements)
+
+    def __len__(self):
+        return len(self._elements)
+
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
 
     def enqueue(self, element):
         self._elements.append(element)
@@ -10,6 +17,7 @@ class Queue:
     def dequeue(self):
         return self._elements.popleft()
 
+# from queues import Queue
 fifo = Queue()
 fifo.enqueue("1st")
 fifo.enqueue("2nd")
@@ -20,4 +28,5 @@ print(fifo.dequeue())
 print(fifo.dequeue())
 
 print(fifo.dequeue())
+
 
