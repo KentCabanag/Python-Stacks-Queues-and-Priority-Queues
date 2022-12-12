baab7fd38b702e254a27af46fd107fe7ac260387
LAPTOP-RQBHQ9EN/Kent Angelo <cabanagkentangelo@gmail.com>
2022-12-12 20:38:03 +0800

    First commit

---
+++ FIFO_Queues.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+class Queue:
+    def __init__(self):
+        self._elements = deque()
+
+    def enqueue(self, element):
+        self._elements.append(element)
+
+    def dequeue(self):
+        return self._elements.popleft()
+
+fifo = Queue()
+fifo.enqueue("1st")
+fifo.enqueue("2nd")
+fifo.enqueue("3rd")
+
+print(fifo.dequeue())
+
+print(fifo.dequeue())
+
+print(fifo.dequeue())
+
