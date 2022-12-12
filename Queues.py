7f22e7526ed6fc3fbdbe85347d0511216857b65e
LAPTOP-RQBHQ9EN/Kent Angelo <cabanagkentangelo@gmail.com>
2022-12-12 22:25:34 +0800

    commit

--- Queues.py
+++ Queues.py
@@ -31,12 +31,18 @@ for element in fifo:
 
 len(fifo)
 
-
+print()
 
 # Building a Stack Data Type
 
 class Stack(Queue):
     def dequeue(self):
         return self._elements.pop()
+
+
+lifo = Stack("1st", "2nd", "3rd")
+for element in lifo:
+    print(element)
+
 
         
