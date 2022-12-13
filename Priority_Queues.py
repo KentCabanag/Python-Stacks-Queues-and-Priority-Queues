fa899da3a9ef0602c1a6ce004b2f884bbd8f387f
LAPTOP-RQBHQ9EN/Kent Angelo <cabanagkentangelo@gmail.com>
2022-12-13 17:47:13 +0800

    Using heapq.heappush()

--- Priority_Queues.py
+++ Priority_Queues.py
@@ -1,2 +1,11 @@
 # Representing Priority Queues With a Heap
+# Using heapq.heappush()
+from heapq import heappush
+
+fruits = []
+heappush(fruits, "orange")
+heappush(fruits, "apple")
+heappush(fruits, "banana")
+
+print(fruits) # Testing the heappush
 
