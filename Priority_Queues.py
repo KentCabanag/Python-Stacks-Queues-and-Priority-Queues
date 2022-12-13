004cd9bdff09f6ea055ffa367ae1ed453d61fd9c
LAPTOP-RQBHQ9EN/Kent Angelo <cabanagkentangelo@gmail.com>
2022-12-13 18:13:53 +0800

    Tuple comparison

--- Priority_Queues.py
+++ Priority_Queues.py
@@ -14,5 +14,13 @@ from heapq import heappop
 
 heappop(fruits)
 
-
 print(fruits)
+
+# Python’s tuple comparison
+person1 = ("John", "Brown", 42)
+person2 = ("John", "Doe", 42)
+person3 = ("John", "Doe", 24)
+
+print(person1 < person2)# Printing if it is True or False
+
+print(person2 < person3)# Printing if it is True or False
