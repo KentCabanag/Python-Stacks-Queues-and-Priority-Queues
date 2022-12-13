c0cdd932061e308cc67b95ac8d31b468b01760c4
LAPTOP-RQBHQ9EN/Kent Angelo <cabanagkentangelo@gmail.com>
2022-12-13 18:46:02 +0800

    Testing Using enqueue_with_priority() method

--- Priority_Queues.py
+++ Priority_Queues.py
@@ -16,6 +16,8 @@ heappop(fruits)
 
 print(fruits)
 
+print()
+
 # Python’s tuple comparison
 person1 = ("John", "Brown", 42)
 person2 = ("John", "Doe", 42)
@@ -24,6 +26,8 @@ person3 = ("John", "Doe", 24)
 print(person1 < person2)# Printing if it is True or False
 
 print(person2 < person3)# Printing if it is True or False
+
+print()
 
 # Building a Priority Queue Data Type
 # Basic priority queue implementation
@@ -40,3 +44,16 @@ class PriorityQueue:
     def dequeue(self):
         return heappop(self._elements)
 
+# from queues import PriorityQueue
+
+CRITICAL = 3
+IMPORTANT = 2
+NEUTRAL = 1
+
+messages = PriorityQueue()
+messages.enqueue_with_priority(IMPORTANT, "Windshield wipers turned on")
+messages.enqueue_with_priority(NEUTRAL, "Radio station tuned in")
+messages.enqueue_with_priority(CRITICAL, "Brake pedal depressed")
+messages.enqueue_with_priority(IMPORTANT, "Hazard lights turned on")
+
+print(messages.dequeue()) # Testing it
