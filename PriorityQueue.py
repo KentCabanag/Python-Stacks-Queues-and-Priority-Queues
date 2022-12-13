f84333043b8b49226c5a1b7f08c0e9e251aae505
LAPTOP-RQBHQ9EN/Kent Angelo <cabanagkentangelo@gmail.com>
2022-12-13 21:36:44 +0800

    Using count() iterator

--- PriorityQueue.py
+++ PriorityQueue.py
@@ -50,3 +50,22 @@ print(messages.dequeue())
 print(messages.dequeue())
 
 print(messages.dequeue())
+
+print()
+
+# Handling Corner Cases in Your Priority Queue
+from dataclasses import dataclass
+
+@dataclass
+class Message:
+    event: str
+
+
+wipers = Message("Windshield wipers turned on")
+hazard_lights = Message("Hazard lights turned on")
+
+wipers < hazard_lights
+
+messages = PriorityQueue()
+messages.enqueue_with_priority(CRITICAL, wipers)
+messages.enqueue_with_priority(IMPORTANT, hazard_lights)
--- queues.py
+++ queues.py
@@ -4,16 +4,19 @@
 # Using enqueue_with_priority() method
 from collections import deque
 from heapq import heappop, heappush
+from itertools import count
 
 class PriorityQueue:
     def __init__(self):
         self._elements = []
+        self._counter = count()       
 
     def enqueue_with_priority(self, priority, value):
-        heappush(self._elements, (-priority, value))
+        element = (-priority, next(self._counter), value)
+        heappush(self._elements, element)
 
     def dequeue(self):
-        return heappop(self._elements)[1]
+        return heappop(self._elements)[-1]
 
 
 
